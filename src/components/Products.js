10a7aed5556b21f6c07417ab20e5cd99829f1f50
Ezequias Aramburu <ezequias@MacBook-Pro-de-Ezequias.local>
2019-10-31 23:39:49 -0300

    add basic query products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { api_url } from '../utils/const';
 
 class Products extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [],
+      extraData: [],
+      page: 1,
+      loading: false,
+    }
   };
 
+  componentDidMount() {
+    const { page } = this.state;
+    this.getProducts(page, 40);
+  };
+
+  getProducts(page, limit) {
+    const { data, extraData} = this.state;
+    if(extraData.length){
+      this.setState({ data: this.state.data.concat(extraData) });
+    }
+    this.setState({ page, limit, loading: true });
+    fetch(api_url).then(resp => resp.json())
+      .then(newData => {
+        if (!data.length) {
+          let extraData = newData.slice(20);
+          newData.length = 20;
+          this.setState({ data: newData, extraData, loading: false });
+        } else {
+          this.setState({ extraData: newData, loading: false });
+        }
+      }).catch(error => {
+        alert('Somenthing went wrong trying to fetch products: ', error.message);
+      });
+  }
+
   render () {
+    const { data } = this.state;
     return (
       <div>
         <div className="table-responsive-sm">
@@ -20,6 +53,22 @@ class Products extends Component {
               </tr>
             </thead>
             <tbody>
+            {data.map((product, index) => {
+              if (index && !(index % 20)) {
+                return this.renderRows(product, index)
+              }
+              else {
+                return ( 
+                  <tr id={index} key={index}>
+                    <th scope="row">{ product.id }</th>
+                    <td style={{ fontSize: product.size }}>{ product.face }</td>
+                    <td>{ product.size }</td>
+                    <td>${ product.price/100 }</td>
+                    <td>{ product.date }</td>
+                  </tr>
+                )}
+              })
+            }
             </tbody>
           </table>
         </div>
---
+++ src/utils/const.js
@@ -0,0 +1,5 @@
+const api_url = 'http://localhost:3000/products';
+
+export {
+  api_url
+};
